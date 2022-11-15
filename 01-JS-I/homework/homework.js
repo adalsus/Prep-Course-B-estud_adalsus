59af380ed3dc62655c8d11253a032d42fa41e039
adalsus <adalbertomonar@hotmail.com>
2022-11-15 17:48:31 -0500

    TAREA 01-JS-I CUMPLIDA!!!

--- 01-JS-I/homework/homework.js
+++ 01-JS-I/homework/homework.js
@@ -1,22 +1,27 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
+//ESTUDIANTE:  adalsus
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = null;
+const nuevaString = 'Híbrido';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 45.06;
 
 // Crea una variable booleana:
-const nuevoBool = null;
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
-const nuevaResta = 10 - null === 5;
+// ¿Qué valor pondrías en vez de null para que la expresión se evalue a verdadera?
+const nuevaResta = 10 - /*null le reemplazo por*/ 5 === 5;
 
 // Resuelve el siguiente problema matemático:
-const nuevaMultiplicacion = 10 * null === 40 ;
+// ¿Qué valor pondrías en vez de null para que la expresión se evalue a verdadera?
+const nuevaMultiplicacion = 10 * /*null le reemplazo por */ 4 === 40 ;
 
 // Resuelve el siguiente problema matemático:
-const nuevoModulo = 21 % 5 === null;
+// ¿Qué valor pondrías en vez de null para que la expresión se evalue a verdadera?
+const nuevoModulo = 21 % 5 === /*null le reemplazo por */ 1;
+//ESTUDIANTE:  adalsus
 
 // No modificar nada debajo de esta línea
 // --------------------------------
